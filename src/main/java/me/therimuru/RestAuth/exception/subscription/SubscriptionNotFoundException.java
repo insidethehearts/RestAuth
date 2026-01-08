5744d87b01c659f1cea75b73bb05d604f871c7d9
insidethehearts <therimuru@mail.ru>
2026-01-08 22:30:24 +0300

    hotfix, community bug report (thanks Kvanzi)

--- src/main/java/me/therimuru/RestAuth/exception/subscription/SubscriptionNotFoundException.java
+++ src/main/java/me/therimuru/RestAuth/exception/subscription/SubscriptionNotFoundException.java
@@ -2,6 +2,6 @@ package me.therimuru.RestAuth.exception.subscription;
 
 public class SubscriptionNotFoundException extends RuntimeException {
     public SubscriptionNotFoundException(Long subscriberId, Long targetId) {
-        super("User wth id %s not subscribed to user with id %s.".formatted(subscriberId, targetId));
+        super("User with id %s not subscribed to user with id %s.".formatted(subscriberId, targetId));
     }
 }
